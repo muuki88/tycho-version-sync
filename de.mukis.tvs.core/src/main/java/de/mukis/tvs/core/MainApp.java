96bf3bdcfbc6f240bc87e18c2c1c665d83af40b3
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-07-13 15:13:16 +0200

    Update build.properties work.

--- de.mukis.tvs.core/src/main/java/de/mukis/tvs/core/MainApp.java
+++ de.mukis.tvs.core/src/main/java/de/mukis/tvs/core/MainApp.java
@@ -8,116 +8,132 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.jar.Manifest;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
 import org.osgi.framework.BundleException;
 
+import de.mukis.tvs.core.models.BuildProperties;
 import de.mukis.tvs.core.models.BundleManifest;
 import de.mukis.tvs.core.models.Project;
 
 public class MainApp {
 
 	private static final String VERSION = "0.0.1";
-	private static final CommandLineParser parser = new PosixParser();
-	private static final Options options = new Options();
-	private static Path root;
+	private static Path root = Paths.get(System.getProperty("user.dir"));
+	private static String help;
 
 	static {
-		options.addOption("h", "help", false, "Show available commmands");
-		options.addOption("v", "version", false, "version number");
-		options.addOption("q", "quit", false, "Quit console");
-
-		options.addOption("l", "list", false, "List all projects");
-		
-		Option project = OptionBuilder.withArgName("project").hasArg().withDescription("Defines the root project directory").create("p");
-
-		options.addOption(project);
-		
-		root = Paths.get(System.getProperty("user.dir"));
+		StringBuilder sb = new StringBuilder();
+		sb.append("[Usage] \n");
+		sb.append(" exit \t Exits the program \n");
+		sb.append(" quit \t Exits the program \n");
+		sb.append(" list \t List all projects \n");
+		sb.append(" version \t Shows programm version \n");
+		help = sb.toString();
 	}
 
 	/**
-	 * @param args
+	 * @param arguments
 	 */
-	public static void main(String[] args) {
-		try(Scanner scanner = new Scanner(System.in)) {
-			CommandLine cmdLine = parser.parse(options, args);
-			execute(cmdLine, scanner);
+	public static void main(String[] arguments) {
+		if (arguments.length == 1) {
+			Path newRoot = Paths.get(arguments[0]);
+			if (Files.exists(newRoot)) {
+				root = newRoot;
+			}
+		}
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Using [" + root + "] as project. Reading projects...");
+			List<Project> projects = readProjects();
+			while (true) {
+				System.out.print(">");
+				String cmd = scanner.next();
+				if (cmd.equals("quit") || cmd.equals("exit")) {
+					System.exit(0);
+				} else if (cmd.equals("version")) {
+					version();
+				} else if (cmd.equals("help")) {
+					help();
+				} else if (cmd.equals("list")) {
+					list(projects);
+				} else if (cmd.equals("qualifier")) {
+					qualifier(scanner, projects);
+				} else {
+					System.out.println(">Unkown command " + cmd);
+					help();
+				}
 
-		} catch (ParseException | IOException | BundleException e) {
+			}
+
+		} catch (Exception e) {
 			System.err.println("[ERROR] " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
-	
-	
-	private static void execute(CommandLine cmdLine, Scanner scanner) throws IOException, BundleException, ParseException {
-		if (cmdLine.hasOption('h'))
-			printHelp(options);
-		if (cmdLine.hasOption('v'))
-			printVersion();
-		if (cmdLine.hasOption('v') || cmdLine.hasOption('h') || cmdLine.hasOption('q'))
-			System.exit(0);
-		if(cmdLine.getArgList().isEmpty())
-			interactive(null, scanner);
-		else
-			interactive(cmdLine, scanner);
+
+	private static void help() {
+		System.out.println(help);
 	}
 
-	private static void printHelp(Options options) {
-		HelpFormatter hf = new HelpFormatter();
-		hf.printHelp("tycho-version-sync", options);
-	}
-
-	private static void printVersion() {
+	private static void version() {
 		System.out.println("Tycho-Version-Sync " + VERSION);
 	}
 
-	private static void interactive(CommandLine cmd, Scanner scanner) throws IOException, BundleException, ParseException {
-		System.out.println("Using [" + root + "] as project");
-		System.out.print(">");
-		CommandLine cmdLine = cmd;
-		if(cmdLine == null) {
-			String first = scanner.next();
-			String[] argsLeft = scanner.nextLine().trim().split(" ");
-			String[] args = new String[argsLeft.length + 1];
-			args[0] = first;
-			System.arraycopy(argsLeft, 0, args, 1, argsLeft.length);
-			
-			cmdLine = parser.parse(options, args);
+	private static void list(List<Project> projects) {
+		for (Project project : projects) {
+			System.out.println(project.toString());
 		}
-		execute(cmdLine, scanner);
 	}
 
+	private static void qualifier(Scanner scanner, List<Project> projects) throws IOException, BundleException {
+		String[] args = scanner.nextLine().trim().split(" ");
+		System.out.println(Arrays.toString(args));
+		if(args.length == 0) {
+			System.out.println("No qualifier given");
+			return;
+		}
+			
+		String version = args[0];
+		for (Project project : projects) {
+			BuildProperties properties = BuildProperties.parse(project.getBuildPropertiesPath());
+			if (properties != null) {
+				properties.setQualifier(version);
+				BuildProperties.write(properties, project.getBuildPropertiesPath());
+			}
+
+			
+			//Manifest mf = BundleManifest.parse(project.getManifestPath());
+			//if (mf != null) {
+			//	OutputStream out = Files.newOutputStream(project.getRoot().resolve("MANIFEST.TEST"));
+			//	mf.write(out);
+			//}
+		}
+
+	}
+
+	private static List<Project> readProjects() throws IOException {
+		ArrayList<Project> projects = new ArrayList<>();
+
+		try (DirectoryStream<Path> projectPaths = Files.newDirectoryStream(root);) {
+			for (Path path : projectPaths) {
+				if ((Files.isDirectory(path) && !path.getFileName().toString().startsWith("."))) {
+					System.out.println("[Found] " + path.getFileName());
+					projects.add(new Project(path));
+				}
+			}
+		}
+		return projects;
+	}
 
 	/*
-	 * 
-	 * List<Project> projects = new LinkedList<>(); try (DirectoryStream<Path>
-	 * stream = Files.newDirectoryStream(root)) { for (Path path : stream) {
-	 * if(Files.isDirectory(path) &&
-	 * !path.getFileName().toString().startsWith(".")) projects.add(new
-	 * Project(path)); } } for (Project project : projects) {
-	 * 
-	 * /// BUILD PROPERTIES BuildProperties properties =
-	 * BuildProperties.parse(project.getBuildPropertiesPath()); if(properties !=
-	 * null) { properties.setQualifier("myNewQualifier");
-	 * BuildProperties.write(properties, project.getBuildPropertiesPath()); }
-	 * 
-	 * 
-	 * Manifest mf = BundleManifest.parse(project.getManifestPath()); if(mf !=
-	 * null) { OutputStream out =
-	 * Files.newOutputStream(project.getRoot().resolve("MANIFEST.TEST"));
-	 * mf.write(out); } }
+	 * private static String[] args(Scanner scanner) { String first =
+	 * scanner.next(); String[] argsLeft = scanner.nextLine().trim().split(" ");
+	 * String[] args = new String[argsLeft.length + 1]; args[0] = first;
+	 * System.arraycopy(argsLeft, 0, args, 1, argsLeft.length); return args; }
 	 */
+
+	// / BUILD PROPERTIES BuildProperties properties =
+
 }
--- de.mukis.tvs.core/src/main/java/de/mukis/tvs/core/models/Project.java
+++ de.mukis.tvs.core/src/main/java/de/mukis/tvs/core/models/Project.java
@@ -1,29 +1,36 @@
 package de.mukis.tvs.core.models;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Project {
 
 	private final Path root;
 
 	private final Path manifestPath;
+	private final Path manifestSearchPath = Paths.get("META-INF", "MANIFEST.MF");
+	
 	private final Path featurePath;
+	private final Path featureSearchPath = Paths.get("feature.xml");
+	
 	private final Path pomPath;
+	private final Path pomSearchPath = Paths.get("pom.xml");
+	
 	private final Path buildPropertiesPath;
+	private final Path buildPropertiesSearchPath = Paths.get("build.properties");
 
 	public Project(Path root) {
 		this.root = root;
-		manifestPath = root.resolve("META-INF").resolve("MANIFEST.MF");
-		featurePath = root.resolve("feature.xml");
-		pomPath = root.resolve("pom.xml");
-		buildPropertiesPath = root.resolve("build.properties");
+		manifestPath = root.resolve(manifestSearchPath);
+		featurePath = root.resolve(featureSearchPath);
+		pomPath = root.resolve(pomSearchPath);
+		buildPropertiesPath = root.resolve(buildPropertiesSearchPath);
 	}
 
 	public Path getRoot() {
 		return root;
 	}
-	
-	
 
 	public Path getManifestPath() {
 		return manifestPath;
@@ -43,8 +50,22 @@ public class Project {
 
 	@Override
 	public String toString() {
-		return "Project [root=" + root + ", manifest=" + manifestPath + ", feature=" + featurePath + ", pom=" + pomPath + ", buildproperties="
-				+ buildPropertiesPath + "]";
+		StringBuilder builder = new StringBuilder();
+		builder.append("Project [").append(root).append("]\n");
+
+		if (manifestPath != null && Files.exists(manifestPath)) {
+			builder.append("- MANIFEST.MF = ").append(manifestSearchPath).append("\n");
+		}
+		if (featurePath != null && Files.exists(featurePath)) {
+			builder.append("- feature.xml = ").append(featureSearchPath).append("\n");
+		}
+		if (pomPath != null && Files.exists(pomPath)) {
+			builder.append("- pom.xml = ").append(pomSearchPath).append("\n");
+		}
+		if (buildPropertiesPath != null && Files.exists(buildPropertiesPath)) {
+			builder.append("- build.properties = ").append(buildPropertiesSearchPath).append("\n");
+		}
+		return builder.toString();
 	}
 
 }
